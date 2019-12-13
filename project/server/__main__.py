c2d4c40d81898191d35647a71af87f2ceeaf8da1
v-v-d <v-v-d@bk.ru>
2019-12-13 18:09:40 +0300

    server side connect description updated

--- project/server/__main__.py
+++ project/server/__main__.py
@@ -11,25 +11,34 @@ parser.add_argument(
 
 args = parser.parse_args()
 
-host = 'localhost'
-port = 8000
+default_config = {
+    'host': 'localhost',
+    'port': 8000,
+    'buffersize': 1024,
+}
 
 if args.config:
     with open(args.config) as file:
-        config = yaml.load(file, Loader=yaml.Loader)
-        host = config.get('host', host)
-        port = config.get('port', port)
+        file_config = yaml.load(file, Loader=yaml.Loader)
+        default_config.update(file_config)
 
-sock = socket()
-sock.bind((host, port,))
-sock.listen(5)
+host, port = (default_config.get('host'), default_config.get('port'))
 
-print(f'Server was started with {host}:{port}')
+try:
 
-while True:
-    client, address = sock.accept()
-    print(f'Client was connected with {address[0]}:{address[1]}')
-    b_request = client.recv(1024)
-    print(f'Client send message: {b_request.decode()}')
-    client.send(b_request)
-    client.close()
+    sock = socket()
+    sock.bind((host, port,))
+    sock.listen(5)
+
+    print(f'Server was started with {host}:{port}')
+
+    while True:
+        client, address = sock.accept()
+        print(f'Client was connected with {address[0]}:{address[1]}')
+        b_request = client.recv(default_config.get('buffersize'))
+        print(f'Client send message: {b_request.decode()}')
+        client.send(b_request)
+        client.close()
+
+except KeyboardInterrupt:
+    print('Server shutdown')
