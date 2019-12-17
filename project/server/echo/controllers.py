ce30e648733c8b16cfe028855bd076d426c530d4
v-v-d <v-v-d@bk.ru>
2019-12-17 23:51:16 +0300

    echo controller wrapped by logged decorator

--- project/server/echo/controllers.py
+++ project/server/echo/controllers.py
@@ -1,6 +1,9 @@
 from protocol import make_response
 
+from decorators import logged
 
+
+@logged
 def echo_controller(request):
     data = request.get('data')
     return make_response(request, 200, data)
