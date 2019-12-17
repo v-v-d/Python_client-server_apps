e03f7f36bf05f0d2b9291f5bd29e6ffdc116cda1
v-v-d <v-v-d@bk.ru>
2019-12-17 23:51:33 +0300

    serverdate controller wrapped by logged decorator

--- project/server/serverdate/controllers.py
+++ project/server/serverdate/controllers.py
@@ -1,7 +1,9 @@
 from datetime import datetime
 
+from decorators import logged
 from protocol import make_response
 
 
+@logged
 def server_date_controller(request):
     return make_response(request, 200, datetime.now().timestamp())
