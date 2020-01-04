884bc5ed8cfad01963dd4cb8bc8e4a57c2a4f3ca
v-v-d <v-v-d@bk.ru>
2020-01-04 18:33:44 +0300

    echo/get_messages_controller serialize bug fixed

--- messenger/server/echo/controllers.py
+++ messenger/server/echo/controllers.py
@@ -1,10 +1,8 @@
-# from functools import reduce
-
 from protocol import make_response
 from database import Session
 from decorators import logged
 
-from models import Message
+from .models import Message
 
 
 @logged
@@ -14,15 +12,12 @@ def echo_controller(request):
     message = Message(data=data)
     session.add(message)
     session.commit()
+    session.close()
     return make_response(request, 200, data)
+
 
 @logged
 def get_messages_controller(request):
     session = Session()
-    messages = [{'data': item.data, 'created': item.created} for item in session.query(Message).all()]
-    # messages = reduce(
-    #     lambda value, item: value + [{'data': item.data, 'created': item.created}],
-    #     session.query(Message).all(),
-    #     []
-    # )
+    messages = [{'data': item.data, 'created': item.created.timestamp()} for item in session.query(Message).all()]
     return make_response(request, 200, messages)
