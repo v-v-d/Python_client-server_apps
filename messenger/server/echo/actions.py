6dc0a997541c730c5a2a4c5fe2e75c10630f3c20
v-v-d <v-v-d@bk.ru>
2020-01-05 02:40:05 +0300

    update and delete actions added to echo/actions.py

--- messenger/server/echo/actions.py
+++ messenger/server/echo/actions.py
@@ -1,4 +1,7 @@
-from .controllers import echo_controller, get_messages_controller
+from .controllers import (
+    echo_controller, get_messages_controller,
+    update_message_controller, delete_message_controller
+)
 
 actionnames = [
     {
@@ -8,5 +11,13 @@ actionnames = [
     {
         'action': 'messages',
         'controller': get_messages_controller,
+    },
+    {
+        'action': 'upd_message',
+        'controller': update_message_controller,
+    },
+    {
+        'action': 'del_message',
+        'controller': delete_message_controller,
     },
 ]
