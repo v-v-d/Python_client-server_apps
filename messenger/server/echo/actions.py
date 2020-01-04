8425c4f5457923256fd9e8a6e38da680096e8515
v-v-d <v-v-d@bk.ru>
2020-01-04 04:26:16 +0300

    new controller named get_messages_controller added to echo/controllers.py

--- messenger/server/echo/actions.py
+++ messenger/server/echo/actions.py
@@ -1,8 +1,12 @@
-from .controllers import echo_controller
+from .controllers import echo_controller, get_messages_controller
 
 actionnames = [
     {
         'action': 'echo',
         'controller': echo_controller,
+    },
+    {
+        'action': 'all_messages',
+        'controller': get_messages_controller,
     },
 ]
--- messenger/server/echo/controllers.py
+++ messenger/server/echo/controllers.py
@@ -1,9 +1,28 @@
-from protocol import make_response
+# from functools import reduce
 
+from protocol import make_response
+from database import Session
 from decorators import logged
+
+from models import Message
 
 
 @logged
 def echo_controller(request):
     data = request.get('data')
+    session = Session()
+    message = Message(data=data)
+    session.add(message)
+    session.commit()
     return make_response(request, 200, data)
+
+@logged
+def get_messages_controller(request):
+    session = Session()
+    messages = [{'data': item.data, 'created': item.created} for item in session.query(Message).all()]
+    # messages = reduce(
+    #     lambda value, item: value + [{'data': item.data, 'created': item.created}],
+    #     session.query(Message).all(),
+    #     []
+    # )
+    return make_response(request, 200, messages)
