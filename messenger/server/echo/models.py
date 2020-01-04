634e5f2081b704a3365ccd4cf906b810fdea20ff
v-v-d <v-v-d@bk.ru>
2020-01-04 18:05:32 +0300

    echo/models.py Message model fields updated

--- messenger/server/echo/models.py
+++ messenger/server/echo/models.py
@@ -3,13 +3,14 @@ from sqlalchemy import Column, Integer, DateTime, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 from database import Base
+from auth.models import User    # Нужен для того, чтобы sqlalchemy увидела эту модель в relationship
 
 
 class Message(Base):
     __tablename__ = 'messages'
     # id = Column(Integer, primary_key=True, autoincrement=True)
     id = Column(Integer, primary_key=True)
-    data = Column(String)
+    data = Column(String, nullable=True)
     created = Column(DateTime, default=datetime.now())
     user_id = Column(Integer, ForeignKey('users.id'))
-    user = relationship('User', back_populates='users')
+    user = relationship('User', back_populates='messages')
