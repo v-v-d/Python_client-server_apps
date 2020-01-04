6c36bc9ec2839291855c417c9d3c5f384f20b9f8
v-v-d <v-v-d@bk.ru>
2020-01-04 20:09:10 +0300

    update_message_controller and delete_message_controller added to echo/controllers

--- messenger/server/echo/controllers.py
+++ messenger/server/echo/controllers.py
@@ -17,6 +17,29 @@ def echo_controller(request):
 
 
 @logged
+def update_message_controller(request):
+    message_data = request.get('data')
+    message_id = request.get('message_id')
+    session = Session()
+    message = session.query(Message).filter_by(id=message_id).first()
+    message.data = message_data
+    session.commit()
+    session.close()
+    return make_response(request, 200)
+
+
+@logged
+def delete_message_controller(request):
+    message_id = request.get('message_id')
+    session = Session()
+    message = session.query(Message).filter_by(id=message_id).first()
+    session.delete(message)
+    session.commit()
+    session.close()
+    return make_response(request, 200)
+
+
+@logged
 def get_messages_controller(request):
     session = Session()
     messages = [{'data': item.data, 'created': item.created.timestamp()} for item in session.query(Message).all()]
