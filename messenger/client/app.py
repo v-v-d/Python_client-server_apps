a99a50b72a59d9f158ff01748457e8f6ddc7e267
v-v-d <v-v-d@bk.ru>
2020-01-11 17:42:51 +0300

    client side encryption-decryption logic added

--- messenger/client/app.py
+++ messenger/client/app.py
@@ -6,9 +6,13 @@ import logging
 from threading import Thread
 from socket import socket
 from datetime import datetime
+
 from PyQt5.QtWidgets import QApplication, QMainWindow
+from Crypto.Cipher import AES
+from Crypto.Random import get_random_bytes
 
 from protocol import make_request
+from utils import get_chunk
 
 
 class TypedProperty:
@@ -47,23 +51,22 @@ class Application:
         if exc_type:
             if exc_type is not KeyboardInterrupt:
                 message = 'Client stopped with error'
-        logging.info(message)
+        logging.info(message, exc_info=exc_val)
         self._socket.close()
         return True
 
     def run(self):
         self._connect()
-        self._read()
-
-        app = QApplication(sys.argv)
-
-        self._render()
-
-        sys.exit(app.exec_())
-
-        # while True:
-        #     self._read()
-        #     self._write()
+        while True:
+            self._read()
+            self._write()
+        # self._read()
+        #
+        # app = QApplication(sys.argv)
+        #
+        # self._render()
+        #
+        # sys.exit(app.exec_())
 
     def _connect(self):
         self._socket.connect((self.host, self.port))
@@ -73,37 +76,54 @@ class Application:
         Thread(target=self._read_by_single_thread).start()
 
     def _read_by_single_thread(self):
-        logging.info(f'Client got response: {self._get_response()}')
+        logging.info(f'Client got response: {self._get_decrypted_response()}')
 
-    def _get_response(self):
-        return zlib.decompress(self._get_compressed_b_response()).decode()
+    def _get_decrypted_response(self):
+        encrypted_response = self._get_encrypted_response()
+        nonce, encrypted_response = get_chunk(encrypted_response, 16)
+        key, encrypted_response = get_chunk(encrypted_response, 16)
+        tag, encrypted_response = get_chunk(encrypted_response, 16)
+        cipher = AES.new(key, AES.MODE_EAX, nonce)
+        return cipher.decrypt_and_verify(encrypted_response, tag).decode()
+
+    def _get_encrypted_response(self):
+        return zlib.decompress(self._get_compressed_b_response())
 
     def _get_compressed_b_response(self):
         return self._socket.recv(self.buffersize)
 
-    def _render(self):
+    @staticmethod
+    def _render():
         window = QMainWindow()
         window.show()
 
-    # def _write(self):
-    #     self._socket.send(self._get_compressed_b_request())
-    #     logging.info(f'Client send request')
-    #
-    # def _get_compressed_b_request(self):
-    #     return zlib.compress(self._get_s_request().encode())
-    #
-    # def _get_s_request(self):
-    #     return json.dumps(self._get_request())
-    #
-    # def _get_request(self):
-    #     action = input('Enter action: ')
-    #     data = input('Enter data: ')
-    #     return make_request(action, data, self._get_token())
-    #
-    # @staticmethod
-    # def _get_token():
-    #     hash_obj = hashlib.sha256()
-    #     hash_obj.update(
-    #         str(datetime.now().timestamp()).encode()
-    #     )
-    #     return hash_obj.hexdigest()
+    def _write(self):
+        self._socket.send(self._get_compressed_b_request())
+        logging.info(f'Client send request')
+
+    def _get_compressed_b_request(self):
+        return zlib.compress(self._get_encrypted_b_request())
+
+    def _get_encrypted_b_request(self):
+        key = get_random_bytes(16)
+        cipher = AES.new(key, AES.MODE_EAX)
+        encrypted_request, tag = cipher.encrypt_and_digest(self._get_b_request())
+        return b'%(nonce)s%(key)s%(tag)s%(data)s' % {
+            b'nonce': cipher.nonce, b'key': key, b'tag': tag, b'data': encrypted_request
+        }
+
+    def _get_b_request(self):
+        return json.dumps(self._get_request()).encode()
+
+    def _get_request(self):
+        action = input('Enter action: ')
+        data = input('Enter data: ')
+        return make_request(action, data, self._get_token())
+
+    @staticmethod
+    def _get_token():
+        hash_obj = hashlib.sha256()
+        hash_obj.update(
+            str(datetime.now().timestamp()).encode()
+        )
+        return hash_obj.hexdigest()
