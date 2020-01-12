77d8a581fceab728657228ba67db3c44928f8ab9
v-v-d <v-v-d@bk.ru>
2020-01-12 04:47:09 +0300

    user and session models updated

--- messenger/server/auth/models.py
+++ messenger/server/auth/models.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+
 from sqlalchemy import Column, Integer, DateTime, String, ForeignKey
 from sqlalchemy.orm import relationship
 
@@ -7,9 +8,8 @@ from database import Base
 
 class User(Base):
     __tablename__ = 'users'
-    # id = Column(Integer, primary_key=True, autoincrement=True)
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String, unique=True, nullable=False)
     password = Column(String, nullable=False)
     sessions = relationship('Session', back_populates='user')
     messages = relationship('Message', back_populates='user')
@@ -17,8 +17,9 @@ class User(Base):
 
 class Session(Base):
     __tablename__ = 'sessions'
-    # id = Column(Integer, primary_key=True, autoincrement=True)
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    token = Column(String, nullable=False, unique=True)
     created = Column(DateTime, default=datetime.now())
+    closed = Column(DateTime, nullable=True)
     user_id = Column(Integer, ForeignKey('users.id'))
     user = relationship('User', back_populates='sessions')
