ebae3fe0f170dd9d95e01b508efb36da5ef5bfed
v-v-d <v-v-d@bk.ru>
2020-01-12 04:44:17 +0300

    database.py session_scope() context manager added

--- messenger/server/database.py
+++ messenger/server/database.py
@@ -1,9 +1,25 @@
+from contextlib import contextmanager
+
 from sqlalchemy import MetaData, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+
 from settings import CONNECTION_STRING
 
 
 engine = create_engine(CONNECTION_STRING)
 Base = declarative_base(metadata=MetaData(bind=engine))
 Session = sessionmaker(bind=engine)
+
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
