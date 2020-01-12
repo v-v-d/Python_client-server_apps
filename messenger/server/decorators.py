e35e7386e7fe90e0bbee89e38a0c8a2af54d82cd
v-v-d <v-v-d@bk.ru>
2020-01-12 04:40:48 +0300

    login_required decorator updated and moved to server/auth/decorators.py

--- messenger/server/decorators.py
+++ messenger/server/decorators.py
@@ -1,8 +1,6 @@
 import logging
 from functools import wraps
 
-from protocol import make_response
-
 
 logger = logging.getLogger('decorators')
 
@@ -11,14 +9,5 @@ def logged(func):
     @wraps(func)
     def wrapper(request, *args, **kwargs):
         logger.debug(f'{func.__name__}: {request}')
-        return func(request, *args, **kwargs)
-    return wrapper
-
-
-def login_required(func):
-    @wraps(func)
-    def wrapper(request, *args, **kwargs):
-        if 'token' not in request:
-            return make_response(request, 403, 'Access denied')
         return func(request, *args, **kwargs)
     return wrapper
