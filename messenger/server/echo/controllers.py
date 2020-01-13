71c0956fb4641c35f7ec5e27893a9e2f42ea81db
v-v-d <v-v-d@bk.ru>
2020-01-14 01:54:19 +0300

    server/echo controllers logic modified via session_scope context manager

--- messenger/server/echo/controllers.py
+++ messenger/server/echo/controllers.py
@@ -1,5 +1,5 @@
 from protocol import make_response
-from database import Session
+from database import Session, session_scope
 from decorators import logged
 
 from .models import Message
@@ -8,42 +8,35 @@ from .models import Message
 @logged
 def echo_controller(request):
     data = request.get('data')
-    session = Session()
-    message = Message(data=data.get('text'))
-    # message = Message(data=data)
-    session.add(message)
-    session.commit()
-    session.close()
+    with session_scope() as session:
+        message = Message(data=data.get('text'))
+        session.add(message)
     return make_response(request, 200, data)
 
 
 @logged
 def update_message_controller(request):
-    message_data = request.get('data')
-    message_id = request.get('message_id')
-    session = Session()
-    message = session.query(Message).filter_by(id=message_id).first()
-    message.data = message_data
-    session.commit()
-    session.close()
+    data = request.get('data')
+    message_data = data.get('text')
+    message_id = data.get('message_id')
+    with session_scope() as session:
+        message = session.query(Message).filter_by(id=message_id).first()
+        message.data = message_data
     return make_response(request, 200)
 
 
 @logged
 def delete_message_controller(request):
-    message_id = request.get('message_id')
-    session = Session()
-    message = session.query(Message).filter_by(id=message_id).first()
-    session.delete(message)
-    session.commit()
-    session.close()
+    data = request.get('data')
+    message_id = data.get('message_id')
+    with session_scope() as session:
+        message = session.query(Message).filter_by(id=message_id).first()
+        session.delete(message)
     return make_response(request, 200)
 
 
 @logged
 def get_messages_controller(request):
-    session = Session()
-    messages = [{'data': item.data, 'created': item.created.timestamp()} for item in session.query(Message).all()]
+    with session_scope() as session:
+        messages = [{'data': item.data, 'created': item.created.timestamp()} for item in session.query(Message).all()]
     return make_response(request, 200, messages)
-
-# TODO: Реализовать контроллеры через session_scope()
