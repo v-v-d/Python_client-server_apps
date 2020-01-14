6a75f8f1a6448b31febb30cdfb3835b6b42b8c99
v-v-d <v-v-d@bk.ru>
2020-01-15 01:44:40 +0300

    server/serverdate docstrings added and remove all wrong models

--- messenger/server/serverdate/controllers.py
+++ messenger/server/serverdate/controllers.py
@@ -1,3 +1,4 @@
+"""Controllers for serverdate module"""
 from datetime import datetime
 
 from decorators import logged
@@ -6,4 +7,5 @@ from protocol import make_response
 
 @logged
 def server_date_controller(request):
+    """Make response based on current date."""
     return make_response(request, 200, datetime.now().timestamp())
