a98fe80eccbe90d24707a685821a3c4fba64efac
v-v-d <v-v-d@bk.ru>
2020-01-15 02:03:40 +0300

    server/__main__ docstrings added

--- messenger/server/__main__.py
+++ messenger/server/__main__.py
@@ -1,3 +1,4 @@
+"""Main script for server side messenger app."""
 import os
 import yaml
 import logging
@@ -11,19 +12,23 @@ from settings import INSTALLED_MODULES, BASE_DIR
 
 
 class ConfigFromCLI:
+    """Get config that passed through CLI."""
     def __init__(self):
+        """Default settings that can be set up by passed through CLI commands."""
         self.is_db_migrated = False
         self._host = '127.0.0.1'
         self._port = 8000
         self._set_config()
 
     def _set_config(self):
+        """Set up the default settings based on passed arguments."""
         config = self._get_config_from_file()
         if config:
             self._host = config.get('host')
             self._port = int(config.get('port'))
 
     def _get_config_from_file(self):
+        """Check for passed arguments ans set up migration default settings."""
         args = self._get_args()
         if args.migrate:
             self.is_db_migrated = True
@@ -33,6 +38,7 @@ class ConfigFromCLI:
 
     @staticmethod
     def _get_args():
+        """Define how a args should be parsed and return args as objects."""
         parser = ArgumentParser()
         parser.add_argument(
             '-c', '--config', type=str,
@@ -45,10 +51,12 @@ class ConfigFromCLI:
 
     @property
     def host(self):
+        """Return app host."""
         return self._host
 
     @property
     def port(self):
+        """Return app port."""
         return self._port
 
 
@@ -63,7 +71,7 @@ logging.basicConfig(
 
 config = ConfigFromCLI()
 if config.is_db_migrated:
-    # Импортируем модули models из всех приложений для того, чтобы выполнить Base.metadata.create_all()
+    # Import all models from all modules for initialize DB migrations (Base.metadata.create_all() method).
     module_name_list = [f'{item}.models' for item in INSTALLED_MODULES]
     module_path_list = (os.path.join(BASE_DIR, item, 'models.py') for item in INSTALLED_MODULES)
     for index, path in enumerate(module_path_list):
