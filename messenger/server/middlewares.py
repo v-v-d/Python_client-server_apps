41c03628205b45c1c8008ed4442f2dcb2d443b19
v-v-d <v-v-d@bk.ru>
2020-01-24 05:20:01 +0300

    server side middlewares.py unused code removed

--- messenger/server/middlewares.py
+++ messenger/server/middlewares.py
@@ -1,12 +1,7 @@
 """Middlewares for server side messenger app."""
 import zlib
-import json
-import hmac
-from functools import wraps
 
-from protocol import make_response
-from auth.models import User
-from database import Session
+from functools import wraps
 
 
 def compression_middleware(func):
@@ -17,25 +12,3 @@ def compression_middleware(func):
         b_response = func(b_request, *args, **kwargs)
         return zlib.compress(b_response)
     return wrapper
-
-
-# def auth_middleware(func):
-#     @wraps(func)
-#     def wrapper(request, *args, **kwargs):
-#         request_obj = json.loads(request)
-#         login = request_obj.get('login')
-#         token = request_obj.get('token')
-#         time = request_obj.get('time')
-#
-#         session = Session()
-#         user = session.query(User).filter_by(name=login).first()
-#
-#         if user:
-#             digest = hmac.new(time, user.password)
-#
-#             if hmac.compare_digest(digest, token):
-#                 return func(request, *args, **kwargs)
-#
-#         response = make_response(request, 401, 'Access denied')
-#         return json.dumps(response).encode()
-#     return wrapper
