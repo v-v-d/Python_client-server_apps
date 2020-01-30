f01c39d52017b2fcb2c27eb8efaae92ca4ffb315
v-v-d <v-v-d@bk.ru>
2020-01-30 03:37:07 +0300

    server side setup.py added

---
+++ messenger/server/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="JI_messenger_server",
+    version="0.0.1",
+    description="Server side of JSON instant messenger",
+    author="Viktor Vasilev",
+    author_email="v-v-d@bk.ru",
+    url="https://github.com/v-v-d/Python_client-server_apps",
+    packages=find_packages(),
+    classifiers=[
+      'Programming Language :: Python :: 3',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+    ],
+)
